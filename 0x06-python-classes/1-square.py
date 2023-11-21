cf6e461e5ba95d2fdaacb6f2bc7cf8eeec201959
SunmolaIdris <sunmolaidris@gmail.com>
2023-11-21 05:40:36 -0800

    Complete tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""File defines a square"""
+
+
+class Square:
+    """Creating a class named Square"""
+    def __init__(self, size=None):
+        """Initializing private attribute size """
+        self.__size = size
+
